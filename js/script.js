68b11254662f202c9eb3525c6d682196f723290e
Nathanbsy <Nathanbs1227@gmail.com>
2024-10-16 14:43:22 -0300

    Adicionando o menu hamburguer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const botaoH = document.getElementById("btn-hamb");
 
+const menuHamb = document.getElementById("menuH");
+
 function mostrarMenu() {
     botaoH.classList.toggle("ativar");
+    menuHamb.classList.toggle("visivel");
 }
